0fc3037d57ea34697dfcd3893077f051673e4a13
konrad <konradhopek@gmail.com>
2013-10-15 15:40:30 +0200

    Create JIn.java

---
+++ JIn.java
@@ -0,0 +1,22 @@
+package lab2.JavaIn;
+
+import java.io.*;
+ 
+public class JIn {
+    public static String getString() {
+	  String text = null;
+	  try{
+		InputStreamReader rd = new InputStreamReader(System.in);
+		BufferedReader bfr = new BufferedReader(rd);
+ 
+		text = bfr.readLine();
+	  }catch(IOException e){e.printStackTrace();}
+      return text;	  
+    }
+    
+    
+    public static int getInteger()
+    {
+		return new Integer(getString());
+	}
+}
